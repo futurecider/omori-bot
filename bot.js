e8684f9c87a18a5a39761d77c568cf04397417da
futurecider <omorimorimo@gmail.com>
2020-02-13 10:33:06 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,7 +87,125 @@ if (message.content.includes('henbot') ) {
 }
 if (message.content === 'omohelp') {
     message.reply('i am just as clueless as you are!(╥︣﹏᷅╥᷅)')
-} // pet thing, possibly youtube thing, and omo commands for point system (omopoints)
+} // music bot time :[
+
+ client.on('message', async message => {
+        if (message.author.bot) return;
+        if (!message.content.startsWith(omo)) return;
+    
+        const serverQueue = queue.get(message.guild.id);
+    
+        if (message.content.startsWith(`${omo}play`)) {
+            execute(message, serverQueue);
+            return;
+        } else if (message.content.startsWith(`${omo}skip`)) {
+            skip(message, serverQueue);
+            return;
+        } else if (message.content.startsWith(`${omo}stop`)) {
+            stop(message, serverQueue);
+            return;
+        } else {
+            message.channel.send('i dont understand!(╥︣﹏᷅╥᷅)')
+        }
+    });
+    async function execute(message, serverQueue) {
+        const args = message.content.split(' ');
+    
+        const voiceChannel = message.member.voiceChannel;
+        if (!voiceChannel) return message.channel.send('get in a voice channel dummy! t(>.<t)');
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+            return message.channel.send('I need the permissions to do that!(╥︣﹏᷅╥᷅)');
+        }
+    
+        const songInfo = await ytdl.getInfo(args[1]);
+        const song = {
+            title: songInfo.title,
+            url: songInfo.video_url,
+        };
+    
+        if (!serverQueue) {
+            const queueContruct = {
+                textChannel: message.channel,
+                voiceChannel: voiceChannel,
+                connection: null,
+                songs: [],
+                volume: 5,
+                playing: true,
+            };
+    
+            queue.set(message.guild.id, queueContruct);
+    
+            queueContruct.songs.push(song);
+    
+            try {
+                var connection = await voiceChannel.join();
+                queueContruct.connection = connection;
+                play(message.guild, queueContruct.songs[0]);
+            } catch (err) {
+                console.log(err);
+                queue.delete(message.guild.id);
+                return message.channel.send(err);
+            }
+        } else {
+            serverQueue.songs.push(song);
+            console.log(serverQueue.songs);
+            return message.channel.send(`${song.title} has been added to the queue (^◡^ )! `);
+        }
+    
+    }
+    
+    function skip(message, serverQueue) {
+        if (!message.member.voiceChannel) return message.channel.send('get in a voice channel dummy! t(>.<t)');
+        if (!serverQueue) return message.channel.send('no song to skip! (╥︣﹏᷅╥᷅)');
+        serverQueue.connection.dispatcher.end();
+    }
+    
+    function stop(message, serverQueue) {
+        if (!message.member.voiceChannel) return message.channel.send('get in a voice channel dummy! t(>.<t)');
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
+    }
+    
+    function play(guild, song) {
+        const serverQueue = queue.get(guild.id);
+    
+        if (!song) {
+            serverQueue.voiceChannel.leave();
+            queue.delete(guild.id);
+            return;
+        }
+    
+        const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+            .on('end', () => {
+                console.log('Music ended!');
+                serverQueue.songs.shift();
+                play(guild, serverQueue.songs[0]);
+            })
+            .on('error', error => {
+                console.error(error);
+            });
+        dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
