9223f0ef40ba07fe32727d8a85cbc6b8c4df0fe9
futurecider <omorimorimo@gmail.com>
2020-02-13 11:47:46 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,10 @@
 const Discord = require('discord.js');
+const {
+	prefix,
+	token,
+} = require('./config.json');
+const ytdl = require('ytdl-core');
+
 const client = new Discord.Client();
 const queue = new Map();
 
